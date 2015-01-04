bea08f1d6f73cc77a1797b1fdc1c4b1f5dbf19b1
Andrew Apollonsky <Anapollonsky@gmail.com>
2015-01-04 12:04:38 -0500

    untabified euler.cpp

--- euler/euler.cpp
+++ euler/euler.cpp
@@ -8,36 +8,36 @@
 using namespace std;
 
 bool isprime(int num, vector<int> primes){
-	for (int i: primes){
-		if (num % i == 0){
-			return false;
-		}
+    for (int i: primes){
+	if (num % i == 0){
+	    return false;
 	}
-	return true;
+    }
+    return true;
 }
 
 vector<int> genprimes(int maxnum){
-	vector<int> primes_list;
-	for(int i = 2; i < maxnum; i++){
-		if (isprime(i, primes_list)){
-			primes_list.push_back(i);
-		}
+    vector<int> primes_list;
+    for(int i = 2; i < maxnum; i++){
+	if (isprime(i, primes_list)){
+	    primes_list.push_back(i);
 	}
-	return primes_list;
+    }
+    return primes_list;
 }
 
 
 template<typename T>
 void printlist(T thelist){
-	for (int i = 0; i < thelist.size(); i++){
-		cout << thelist[i] << " ";
-	}
-	cout << endl;
+    for (int i = 0; i < thelist.size(); i++){
+	cout << thelist[i] << " ";
+    }
+    cout << endl;
 }
 
 
 int main(void) {
-	vector<int> primes = genprimes(10000);
-	vector<string> vec {"hey", "they"};
-	printlist(primes);
+    vector<int> primes = genprimes(10000);
+    vector<string> vec {"hey", "they"};
+    printlist(primes);
 }
