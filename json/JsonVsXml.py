96da3b13679331ada17385931444f11a0e3d029a
Andrew Apollonsky <Anapollonsky@gmail.com>
2015-01-04 01:36:57 -0500

    Added exercises.org, jsonvsxml tests.

---
+++ json/JsonVsXml.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import json
+import jsonpickle
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+class CalRecord:
+    
+    def __init__(self, name, recid):
+        self.name = name
+        self.recid = recid
+        self.axisdata = []
+        self.singdata = []
+        
+    def add_axis(self, axis):
+        self.axisdata.append(axis)
+
+    def add_singular(self, sing):
+        self.singdata.append(sing)
+
+    def print_axes(self):
+        for k in self.axisdata:
+            print(k.axisname + ": " + str(k.axisdata))
+
+    def print_singular(self):
+        for k in self.singdata:
+            print(k.singname + ": " + str(k.singdata)) 
+
+    # http://stackoverflow.com/questions/3768895/python-how-to-make-a-class-json-serializable
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    
+class Axis:
+    def __init__(self, name, data):
+        self.axisname = name
+        self.axisdata = data
+
+class Singular:
+    def __init__(self, name, data):
+        self.singname = name
+        self.singdata = data
+
+# Create object        
+Record1 = CalRecord("TXFREQ", "17")
+Record1.add_axis(Axis("Frequency", [1,2,3,4,5]))
+Record1.add_axis(Axis("Magic Dissipation", [1,2,3,4,7]))
+Record1.add_singular(Singular("Who:?", 5))
+
+# Write to file
+f = open('testfile', 'w+')
+f.write(Record1.to_JSON())
+f.close()
+
+print json.dumps(Record1.__dict__)
---
+++ json/jsondecode.cpp
@@ -0,0 +1,9 @@
+// http://uscilab.github.io/cereal/quickstart.html
+#include <cereal/archives/binary.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
