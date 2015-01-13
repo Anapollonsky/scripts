2a9dd803e3e312cbf2eda6b63aa441242baacb8d
Andrew Apollonsky <anapollonsky@gmail.com>
2015-01-13 14:53:42 -0500

    Added unzipping function and some error handling.

--- xml/filter_eeprom_reader.cpp
+++ xml/filter_eeprom_reader.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
 #include "./rapidxml/rapidxml.hpp"
 #include "./rapidxml/rapidxml_print.hpp"
 #include "./rapidxml/rapidxml_utils.hpp"
@@ -8,17 +11,25 @@
 
 #include "i2c_eeprom.c"
 
-
-
-
-
 using namespace std;
 
 // Example:  /vobs/linuxrru/usl/fd_rrh_ca1/i2cfd_rrh_ca1/i2c_bci.c:222
 UINT8 filter_records_buffer[FILTER_RECORD_LENGTH];
 memset(filter_records_buffer, 0, sizeof(filter_records_buffer));
-filter_eeprom_read(0, 0, CONFIG_SYS_FILTER_EEPROM_SIZE, filter_records_buffer);
+
+if (!filter_eeprom_read(0, 0, CONFIG_SYS_FILTER_EEPROM_SIZE, filter_records_buffer)){
+    _ERR("Reading filter eeprom contents failed.");
+    return ERROR;
+}
 char filename[]="/tmp/rxadc.bin"; // /vobs/rru/ral/sacapt/SACapture.cc:300
+
+char* fname = "eeprom_filter_records.xml.gz";
+
+if (!writeBufferToFile(fname, filter_records_buffer, CONFIG_SYS_FILTER_EEPROM_SIZE)){
+    _ERR("Writing filter record file failed.");
+    return ERROR;
+}
+    
 
 // from int SACapture::writeBufferToFile(char *filename, UINT32 nSamples)
 // /vobs/rru/ral/sacapt/SACapture.cc:251
@@ -26,7 +37,6 @@ int writeBufferToFile(char *filename, UINT8 buffer, UINT8 buffer_entries){
     FILE *fp;
     UINT32 readBytes, bufLength, size;
     size = sizeof(UINT8);
-//    bufLEngth = size
     fp = fopen(filename, "wb");
     if (fp == NULL)
     {
@@ -47,15 +57,23 @@ int writeBufferToFile(char *filename, UINT8 buffer, UINT8 buffer_entries){
 
 // http://stackoverflow.com/questions/8465006/how-to-concatenate-2-strings-in-c
 int command_unzip(char *filename){
-    char* precommand = "gunzip ";
-    char *command = malloc(strlen(precommand) + strlen(filename) + 1);
-    strcpy(command, precommand);
-    // strcat(
-    // system("gunzip" 
-
+    char *precommand = "gunzip ";
+    char *command = (char*)malloc(strlen(precommand) + strlen(filename) + 1);
+    if (command != NULL){
+	strncpy(command, precommand);
+	strncat(command, filename);
+    } else {
+	_ERR("cannot allocate memory for gunzip command.\n");
+	return ERROR;
+    }
+    if (system(&command) == ERROR){
+	_ERR("Ungzipping system call failed.");
+	return ERROR;
+    }
+    return OK;
 }
 
-void traverse_xml(void) {
+int parse_xml(void) {
     rapidxml::file<> xmlFile("records.xml");
     rapidxml::xml_document<> doc;
     doc.parse<rapidxml::parse_declaration_node | rapidxml::parse_no_data_nodes>(xmlFile.data());
@@ -82,6 +100,7 @@ void traverse_xml(void) {
         }
         record = record->next_sibling("record");
     }
+    return OK;
 }
 
 
@@ -90,7 +109,7 @@ int main(int argc, char *argv[]) {
     // FILE *input = fopen("records.xml.gz", "gz");
     
     
-    traverse_xml();    
+    parse_xml();    
     return 0;
 }
 
